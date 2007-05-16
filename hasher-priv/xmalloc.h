ea327521afc478a96c18c4a83eaec54ca2837b56
Dmitry V. Levin <ldv@altlinux.org>
2007-05-16 10:28:17 +0000

    xmalloc.h (xasprintf): Add __nonnull__(2) attribute

--- hasher-priv/xmalloc.h
+++ hasher-priv/xmalloc.h
@@ -26,6 +26,7 @@ extern void *xmalloc(size_t size);
 extern void *xrealloc(void *ptr, size_t size);
 extern char *xstrdup(const char *s);
 extern char *xasprintf(char **ptr, const char *fmt, ...)
-	__attribute__ ((__format__(__printf__, 2, 3)));
+	__attribute__ ((__format__(__printf__, 2, 3)))
+	__attribute__ ((__nonnull__(2)));
 
 #endif /* __XMALLOC_H__ */
